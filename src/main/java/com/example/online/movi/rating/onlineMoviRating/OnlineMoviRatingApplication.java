68321ef0db73b5653ea4349ab7d84935191e7cb5
ASUS <pavanshetty05@gmail.com>
2023-04-30 12:31:25 +0530

    Second commit

---
+++ src/main/java/com/example/online/movi/rating/onlineMoviRating/OnlineMoviRatingApplication.java
@@ -0,0 +1,13 @@
+package com.example.online.movi.rating.onlineMoviRating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMoviRatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineMoviRatingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/online/movi/rating/onlineMoviRating/OnlineMoviRatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online.movi.rating.onlineMoviRating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMoviRatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
