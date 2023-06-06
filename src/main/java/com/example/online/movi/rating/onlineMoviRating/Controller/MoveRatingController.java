510b42c1458441ab47528cade0f183a3c0b9da2d
ASUS <pavanshetty05@gmail.com>
2023-06-06 19:00:42 +0530

    Commit and push

--- src/main/java/com/example/online/movi/rating/onlineMoviRating/Controller/MoveRatingController.java
+++ src/main/java/com/example/online/movi/rating/onlineMoviRating/Controller/MoveRatingController.java
@@ -1,5 +1,7 @@
 package com.example.online.movi.rating.onlineMoviRating.Controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,28 +14,40 @@ import com.example.online.movi.rating.onlineMoviRating.Module.MoviRating;
 @RestController
 @RequestMapping(value = "/movi")
 public class MoveRatingController {
+	
+	Logger log = 
+	        LoggerFactory.getLogger(MoveRatingController.class);
 
 	@RequestMapping(value = "/rating", method = RequestMethod.POST, consumes = "application/json")
 	public @ResponseBody MoviRating addMoveDetails(@RequestBody MoviRating ouser) {
+		
+		log.info("Add move");
 
 		return ouser;
 	}
 
-	@RequestMapping(value = "/rating", method = RequestMethod.GET, consumes = "application/json")
-	public @ResponseBody MoviRating getMoveDetails(@PathVariable String moveID) {
+	@RequestMapping(value = "/rating", method = RequestMethod.GET)
+	public @ResponseBody MoviRating getMoveDetails() {
+		
+		log.info("Get move");
 
-		MoviRating ouserMapping = new MoviRating(moveID, null);
+		MoviRating ouserMapping = new MoviRating();
+		ouserMapping.setMoveID("pavan");
+		ouserMapping.setMoverating("pavan");
 		return ouserMapping;
 	}
 
 	@RequestMapping(value = "/rating", method = RequestMethod.DELETE, consumes = "application/json")
 	public @ResponseBody MoviRating removeDetails(@RequestBody MoviRating ouser) {
 
+		log.info("Remove move");
 		return ouser;
 	}
 
 	@RequestMapping(value = "/rating", method = RequestMethod.PUT, consumes = "application/json")
 	public @ResponseBody MoviRating updateMoveDetails(@RequestBody MoviRating ouser) {
+		
+		log.info("Update move");
 
 		return ouser;
 	}
--- src/main/java/com/example/online/movi/rating/onlineMoviRating/OnlineMoviRatingApplication.java
+++ src/main/java/com/example/online/movi/rating/onlineMoviRating/OnlineMoviRatingApplication.java
@@ -1,5 +1,7 @@
 package com.example.online.movi.rating.onlineMoviRating;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,6 +11,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class OnlineMoviRatingApplication {
 
 	public static void main(String[] args) {
+		Logger log = 
+		        LoggerFactory.getLogger(OnlineMoviRatingApplication.class);
 		SpringApplication.run(OnlineMoviRatingApplication.class, args);
 	}
 
